7da09365f8c75bb22f34968830d4bc42f42da2cc
MyNguyen12345 <mynguyen04012000@gmail.com>
2022-11-08 15:23:31 +0700

    fix

--- apiDATN/src/main/kotlin/com/example/apidatn/constant/Constant.kt
+++ apiDATN/src/main/kotlin/com/example/apidatn/constant/Constant.kt
@@ -1,7 +1,7 @@
 package com.example.apidatn.constant
 
 object Constant {
-    const val fileName ="com/example/apidatn/constant/signkey"
+    const val fileName ="apiDATN/src/main/kotlin/com/example/apidatn/constant/signkey"
     const val ACCESS_TOKEN_VALIDITY_SECONDS = 5 * 60 * 60
     const val AUTHORITIES_KEY = "scopes"
 }
