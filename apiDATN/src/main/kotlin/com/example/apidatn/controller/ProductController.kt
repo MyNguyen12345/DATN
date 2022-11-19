0ea0dc01a67d6241c9aaf153d032bdbd513e5263
MyNguyen12345 <mynguyen04012000@gmail.com>
2022-11-19 18:15:35 +0700

    feature/test_product_utf8

--- apiDATN/src/main/kotlin/com/example/apidatn/controller/ProductController.kt
+++ apiDATN/src/main/kotlin/com/example/apidatn/controller/ProductController.kt
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*
 import org.springframework.web.multipart.MultipartFile
 
 @RestController
-@RequestMapping("/product",produces = ["application/json"])
+@RequestMapping("/product",produces = ["application/json;charset=utf8"])
 class ProductController {
 
     @Autowired
