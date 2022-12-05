d0930b0aea923f4cbd1e670b00d3162e7e1490bc
MyNguyen12345 <mynguyen04012000@gmail.com>
2022-12-05 15:42:04 +0700

    feature/fix feature cart product bill

--- apiDATN/src/main/kotlin/com/example/apidatn/service/CartServiceImpl.kt
+++ apiDATN/src/main/kotlin/com/example/apidatn/service/CartServiceImpl.kt
@@ -20,6 +20,7 @@ class CartServiceImpl: CartService {
     fun toDtoEntity(cartDto: CartDto): Cart =mapper.map(cartDto, Cart::class.java)
 
     fun toEntityDto(cart: Cart): CartDto = mapper.map(cart, CartDto::class.java)
+    fun toEntityDtoProduct(product: Product): ProductDto = mapper.map(product, ProductDto::class.java)
     fun toEntityDtoUser(user: User): UserInfoDto = mapper.map(user, UserInfoDto::class.java)
 
     @Autowired
@@ -61,25 +62,40 @@ class CartServiceImpl: CartService {
 
         var list:MutableList<ListCartDto> = mutableListOf()
         var product0=productRepository.findById(listcart[0].productId!!).get()
-            list.add(
-                    ListCartDto(
-                            listCart= product0?.userId?.let { cartRepository.listCartUserId(it) }
-                                    ?.stream()?.map { cart:Cart->toEntityDto(cart)}?.collect(Collectors.toList()),
-                            user = toEntityDtoUser(userRepository.findById(product0?.userId!!).get())
-                    )
-            )
+        var listCartDe=product0?.userId?.let { cartRepository.listCartUserId(it) }
+                ?.stream()?.map { cart:Cart->toEntityDto(cart)}?.collect(Collectors.toList())
+        if (listCartDe != null) {
+            for (cart in listCartDe){
+                cart.product= cart.productId?.let { productRepository.findById(it).get() }?.let { toEntityDtoProduct(it) }
+
+            }
+                list.add(
+                        ListCartDto(
+                                listCart=listCartDe ,
+                                user = toEntityDtoUser(userRepository.findById(product0?.userId!!).get())
+                        )
+                )
+        }
         for (index in listcart.size-1 downTo 1){
             var product2=productRepository.findById(listcart[index].productId!!).get()
             var product3=productRepository.findById(listcart[index-1].productId!!).get()
 
             if(product2.userId!=product3.userId){
-                list.add(
-                        ListCartDto(
-                                listCart= product2?.userId?.let { cartRepository.listCartUserId(it) }
-                                        ?.stream()?.map { cart:Cart->toEntityDto(cart)}?.collect(Collectors.toList()),
-                                user = toEntityDtoUser(userRepository.findById(product2?.userId!!).get())
-                        )
-                )
+
+                var listCartDe=product2?.userId?.let { cartRepository.listCartUserId(it) }
+                        ?.stream()?.map { cart:Cart->toEntityDto(cart)}?.collect(Collectors.toList())
+                if (listCartDe != null) {
+                    for (cart in listCartDe){
+                        cart.product= cart.productId?.let { productRepository.findById(it).get() }?.let { toEntityDtoProduct(it) }
+
+                    }
+                    list.add(
+                            ListCartDto(
+                                    listCart=listCartDe ,
+                                    user = toEntityDtoUser(userRepository.findById(product2?.userId!!).get())
+                            )
+                    )
+                }
             }
         }
         return list
