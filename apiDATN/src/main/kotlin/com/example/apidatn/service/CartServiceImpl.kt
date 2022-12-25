c811826b30d2d3f79ec6f061e74a537a11a59cc3
MyNguyen12345 <mynguyen04012000@gmail.com>
2022-12-25 15:14:31 +0700

    feature/fix feature cart

--- apiDATN/src/main/kotlin/com/example/apidatn/service/CartServiceImpl.kt
+++ apiDATN/src/main/kotlin/com/example/apidatn/service/CartServiceImpl.kt
@@ -78,46 +78,49 @@ class CartServiceImpl: CartService {
 
         var listId:MutableList<Int> = mutableListOf()
         var boolean=false
-       listcart[listcart.size-1].product?.userId?.let { listId.add(it) }
+        if(listcart.size>0){
+            listcart[listcart.size-1].product?.userId?.let { listId.add(it) }
 
-        for (index in listcart.size-1 downTo 0) {
-            boolean=false
-            var userId2 = listcart[index].product?.userId
-            for (i in listId.size - 1 downTo 0) {
-                var userId3 =listId[i]
-                if (userId2 == userId3) {
-                    boolean= true
-                    break
-                }
-            }
-            if (!boolean){
-                userId2?.let { listId.add(it) }
-            }
-        }
-        for (userID in listId){
-            val listCartDe=userID?.let { cartRepository.listCartUserId(it) }
-                    ?.stream()?.map { cart:Cart->toEntityDto(cart)}?.collect(Collectors.toList())
-
-            if (listCartDe != null) {
-                for (cart in listCartDe){
-                    cart.product= cart.productId?.let { productRepository.findById(it).get() }?.let { toEntityDtoProduct(it) }
-                    if(ratingRepository.findByProductId(cart.product?.productId!!).size>0){
-                        cart.product?.rating = ratingRepository.avgRating(cart.product?.productId!!)
-                        cart.product?.userRating= ratingRepository.amountRatingByUser(cart.product?.productId!!)
-                    }else{
-                        cart.product?.rating =0F
-                        cart.product?.userRating=0
+            for (index in listcart.size-1 downTo 0) {
+                boolean=false
+                var userId2 = listcart[index].product?.userId
+                for (i in listId.size - 1 downTo 0) {
+                    var userId3 =listId[i]
+                    if (userId2 == userId3) {
+                        boolean= true
+                        break
                     }
-
                 }
-                list.add(
-                        ListCartDto(
-                                listCart=listCartDe ,
-                                user = toEntityDtoUser(userRepository.findById(userID!!).get())
-                        )
-                )
+                if (!boolean){
+                    userId2?.let { listId.add(it) }
+                }
+            }
+            for (userID in listId){
+                val listCartDe=userID?.let { cartRepository.listCartUserId(it) }
+                        ?.stream()?.map { cart:Cart->toEntityDto(cart)}?.collect(Collectors.toList())
+
+                if (listCartDe != null) {
+                    for (cart in listCartDe){
+                        cart.product= cart.productId?.let { productRepository.findById(it).get() }?.let { toEntityDtoProduct(it) }
+                        if(ratingRepository.findByProductId(cart.product?.productId!!).size>0){
+                            cart.product?.rating = ratingRepository.avgRating(cart.product?.productId!!)
+                            cart.product?.userRating= ratingRepository.amountRatingByUser(cart.product?.productId!!)
+                        }else{
+                            cart.product?.rating =0F
+                            cart.product?.userRating=0
+                        }
+
+                    }
+                    list.add(
+                            ListCartDto(
+                                    listCart=listCartDe ,
+                                    user = toEntityDtoUser(userRepository.findById(userID!!).get())
+                            )
+                    )
+                }
             }
         }
+
         return list
     }
 
